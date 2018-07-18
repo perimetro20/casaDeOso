ca73fa079838cab8f355b87743647e1a9076c542
Perimetro20 <a01205778@itesm.mx>
2018-07-18 12:53:55 -0500

    Allows the hosts

--- hyrisa/settings/production.py
+++ hyrisa/settings/production.py
@@ -4,6 +4,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['YOUR_DOMAIN(S)_GO_HERE']
+ALLOWED_HOSTS = ['hyrisa.perimetro20.com', '138.197.202.211']
 
 STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
