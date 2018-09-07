39f77968937a809c9b10857d83bfe4d8fd9592df
Perimetro20 <a01205778@itesm.mx>
2018-09-07 17:20:25 -0500

    Change the allowed hosts config in production

--- oso/settings/production.py
+++ oso/settings/production.py
@@ -4,6 +4,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hyrisa.perimetro20.com', '138.197.202.211']
+ALLOWED_HOSTS = ['oso.perimetro20.com', '138.197.202.211']
 
 STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
