5d66ca193a94191a9095b66201b39bec56ce3524
Perimetro20 <a01205778@itesm.mx>
2018-09-07 03:19:31 -0500

    Delete all proper files

--- casa_de_oso/settings/production.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from .base import *
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['hyrisa.perimetro20.com', '138.197.202.211']
-
-STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
